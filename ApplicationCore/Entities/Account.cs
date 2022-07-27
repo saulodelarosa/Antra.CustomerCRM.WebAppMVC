ea61fe0a606b4d554e43a9c3b8f14a23915724f8
sadelarosa <wowcheckitphone@gmail.com>
2022-07-27 15:02:26 -0400

    update

--- ApplicationCore/Entities/Account.cs
+++ ApplicationCore/Entities/Account.cs
@@ -2,5 +2,7 @@
 {
     public class Account
     {
+        public int Id { get; set; }
+        public int Balance { get; set; }
     }
 }
--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Antra.CustomerCRM.WebAppMVC.ApplicationCore.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Antra.CustomerCRM.WebAppMVC.Controllers
 {
@@ -6,7 +7,15 @@ namespace Antra.CustomerCRM.WebAppMVC.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            ViewBag.UserName = "test";
+
+            List<Account> lstAccounts = new List<Account>
+            {
+                new Account() {Id = 1, Balance = 50},
+                new Account() {Id = 2, Balance = 90}
+            };
+
+            return View(lstAccounts);
         }
     }
 }
