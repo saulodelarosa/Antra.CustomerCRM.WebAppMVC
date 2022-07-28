1573126306756e4dfe2b5ebd106bd8a064f1a8f1
sadelarosa <wowcheckitphone@gmail.com>
2022-07-28 11:08:25 -0400

    update

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSqlServer<CustomerDbContext>(builder.Configuration.GetConnectionString("CustomerCRM");
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
